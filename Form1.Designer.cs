cfcaf610c6a5b1c6a5b3ff21179ce7ec243fccb4
Adalberto <adalbertofm1995@gmail.com>
2024-01-11 15:56:02 -0600

    CI: Access screen

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,51 @@ namespace KioscoConsulta
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.BtIngreso = new System.Windows.Forms.Button();
+            this.TbUsuario = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BtIngreso
+            // 
+            this.BtIngreso.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtIngreso.Location = new System.Drawing.Point(246, 521);
+            this.BtIngreso.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.BtIngreso.Name = "BtIngreso";
+            this.BtIngreso.Size = new System.Drawing.Size(286, 100);
+            this.BtIngreso.TabIndex = 0;
+            this.BtIngreso.Text = "INGRESAR";
+            this.BtIngreso.UseVisualStyleBackColor = true;
+            // 
+            // TbUsuario
+            // 
+            this.TbUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TbUsuario.Location = new System.Drawing.Point(39, 541);
+            this.TbUsuario.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.TbUsuario.Name = "TbUsuario";
+            this.TbUsuario.Size = new System.Drawing.Size(178, 62);
+            this.TbUsuario.TabIndex = 1;
+            this.TbUsuario.UseSystemPasswordChar = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1370, 749);
+            this.Controls.Add(this.TbUsuario);
+            this.Controls.Add(this.BtIngreso);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button BtIngreso;
+        private System.Windows.Forms.TextBox TbUsuario;
     }
 }
 
